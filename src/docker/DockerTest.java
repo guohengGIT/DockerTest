6e4983650eb0e0947206f21721c42257a32a39d6
heng.guo <heng.guo@hpe.com>
2016-07-23 19:16:32 +0800

    first add all files

---
+++ src/docker/DockerTest.java
@@ -0,0 +1,23 @@
+package docker;
+
+import java.io.File;
+//import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DockerTest {
+	public static void main(String[] args) throws IOException {
+		File f = new File("E:\\DevOps\\Docker\\test\\test.txt");
+		if(!f.exists()){
+			f.createNewFile();
+			System.out.println("success");
+		}
+		FileWriter fw = new FileWriter(f);
+//		FileOutputStream fos = new FileOutputStream(fw);
+		
+//		byte [] str = {1,2,3};
+		fw.append("test");
+		fw.flush();
+		fw.close();
+	}
+}
